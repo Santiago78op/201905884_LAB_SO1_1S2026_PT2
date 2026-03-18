eb1edea68eadf42cca9d67146e8d3fe03a0f1702
Santiago78op <2993696170101@ingenieria.usac.edu.gt>
2026-03-17 20:02:23 -0600

    Create a new module kernel rust

--- Rust_Module/module_kernel.rs
+++ Rust_Module/module_kernel.rs
@@ -1,36 +1,28 @@
-#![no_std]
-#![feature(custom_attribute)] // Ya no funciona
-#![feature(abi_x86_interrupt)]
+// SPDX-License-Identifier: GPL-2.0
 
+use kernel::prelude::*;
 
-// src/lib.rs
-/** 
- * Rust Module for Linux Kernel
- * Author: 201905884
- * 
- * no_std: This attribute indicates that the Rust standard library is not linked to the crate.
- * 
- * custom_attributes: This feature allows the use of custom attributes in the code, 
- * which can be used for various purposes such as marking functions or 
- * data structures with specific metadata.
- * 
- * abi_x86_interrupt: This feature enables the use of the x86 interrupt calling convention,
- * which is necessary for writing interrupt handlers in Rust for the Linux kernel.
- * 
-*/
-
-use core::ffi::c_void;
-use kernel_module::{c_types, module::{init_module, cleanup_module}};
-
-#[init]
-fn on_init() -> c_types::c_int {
-    println!("Rust module init");
-    println!("Hello, Kernel! 201905884");
-    0 // Return 0 to indicate successful initialization
+module! {
+    type: RustHola,
+    name: "rust_hola",
+    authors: ["Julian Reyes"],
+    description: "Modulo Linux en Rust: hola mundo",
+    license: "GPL",
 }
 
-#[cleanup]
-fn on_cleanup() {
-    println!("Rust module cleanup");
+struct RustHola;
+
+impl kernel::Module for RustHola {
+    fn init(_module: &'static ThisModule) -> Result<Self> {
+        pr_info!("Hola mundo desde un modulo Linux en Rust\n");
+        pr_info!("Saluda Carnet: 201905884\n");
+        Ok(RustHola)
+    }
 }
 
+impl Drop for RustHola {
+    fn drop(&mut self) {
+        pr_info!("Adios desde rust_hola\n");
+        pr_info!("Adios Carnet: 201905884\n");
+    }
+}
